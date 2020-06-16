748e261599834c76a44e7db00e21c23ca749dc6a
sejalbandil <51757914+sejalbandil@users.noreply.github.com>
2020-06-16 10:54:17 -0700

    Update age.py

--- age.py
+++ age.py
@@ -1,4 +1,4 @@
-
+#This code asks the user to enter their name and their age. Then it prints out a message that tells them the year that they will turn 100 years old!
 name = raw_input ("Enter your name: ")
 
 age = input ("Enter your age: ")
