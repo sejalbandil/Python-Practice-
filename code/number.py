929bc0771b3572b639d6b8f5ffbb7194408c4be8
sejalbandil <51757914+sejalbandil@users.noreply.github.com>
2020-07-01 20:11:09 -0700

    Delete number.py

--- code/number.py
+++
@@ -1,9 +0,0 @@
-numbers = list()
-filename = open("input.txt")
-outputfile= open("output2.txt", "a")
-outputfile.truncate(0)
-for line in sorted(filename, key = lambda line: int(line.split(",")[1])):
-    outputfile.write (str(line))
-    outputfile.write ("\n")
-
-outputfile.close()
