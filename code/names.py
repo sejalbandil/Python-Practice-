47f99c08164c970da22faa17af512093a43c736a
sejalbandil <51757914+sejalbandil@users.noreply.github.com>
2020-07-01 20:11:00 -0700

    Delete names.py

--- code/names.py
+++
@@ -1,9 +0,0 @@
-names = list()
-filename = open("input.txt")
-outputfile= open("output1.txt", "a")
-outputfile.truncate(0)
-for line in sorted(filename, key = lambda line: line.split(",")[0]):
-    outputfile.write (str(line))
-    outputfile.write ("\n")
-
-outputfile.close()
