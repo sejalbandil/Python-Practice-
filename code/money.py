2f9bc7fe2086d2d921c3dbb9b3c768695961b43b
sejalbandil <51757914+sejalbandil@users.noreply.github.com>
2020-07-01 20:10:50 -0700

    Delete money.py

--- code/money.py
+++
@@ -1,9 +0,0 @@
-numbers = list()
-filename = open("input.txt")
-outputfile= open("output3.txt", "a")
-outputfile.truncate(0)
-for line in sorted(filename, key = lambda line: int(line.replace("$","").split(",")[2])):
-    outputfile.write (str(line))
-    outputfile.write ("\n")
-
-outputfile.close()
